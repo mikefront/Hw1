50dffe3191ae7bf09b6efa4328d83e4ed7be8493
mikefront <mike_front@mail.ru>
2023-08-17 13:57:31 +0500

    Задание5

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1() {
@@ -59,6 +60,9 @@ public class Main {
         System.out.println("paper-" + paper);
     }
     public static void task4() {
+        System.out.println("Задание4");
+        {
+        }
         var friend = 19;
         System.out.println("friend-" + friend);
         {
@@ -71,5 +75,10 @@ public class Main {
         var friend2 = 38.0;
         friend2 = friend2 / 7.0;
         System.out.println("friend-" + friend2);
+    }
+    public static void task5() {
+        System.out.println("Задание5");
+        {
+        }
     }
 }
