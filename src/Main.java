5aaafcfaa12863e245accc0c2a75b736e63a3d23
mikefront <mike_front@mail.ru>
2023-08-17 11:50:28 +0500

    Задание1 сделано

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задание1");
+        {
+        }
+        var dog = 8.0;
+        System.out.println("dog-" + dog);
+        {
+        }
+        var cat = 3.6;
+        System.out.println("cat-" + cat);
+        {
+        }
+        var paper = 763789;
+        System.out.println("paper-" + paper);
     }
 }
